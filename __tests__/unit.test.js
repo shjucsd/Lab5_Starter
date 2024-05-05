e2892b7cd607a307a7ad40199cdbdf5fdbb2979f
shjucsd <125161884+shjucsd@users.noreply.github.com>
2024-05-04 23:41:19 -0700

    Update unit.test.js

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -15,7 +15,7 @@ import {
 
 //test1
 test('checks correctly if correct phone number', () => {
-  expect(isPhoneNumber('655-555-0100')).toBe(true);
+  expect(isPhoneNumber('655-555-0100')).toBe(false);
 });
 
 //test2
