dae9b50db7591c1de42dea04dfcb3b71cd91ca2b
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-05-06 18:16:32 -0700

    set up explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,50 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
 
 function init() {
-  // TODO
+  populateVoiceList();
+  updateFace();
+
+}
+function populateVoiceList() {
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+}
+
+function updateFace() {
+  const face_img = (document.getElementById("explore")).querySelector("img");
+  setTimeout(() => {
+    if (synth.speaking) {
+      // console.log
+      face_img.setAttribute('src', 'assets/images/smiling-open.png');
+      updateFace();
+    } else {
+      face_img.setAttribute('src', 'assets/images/smiling.png');
+    }
+  }, 200);
+}
+
+populateVoiceList();
+if (
+  typeof speechSynthesis !== "undefined" &&
+  speechSynthesis.onvoiceschanged !== undefined
+) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
