ced1173144c20b3ed616b2bb94fbddd91c57d493
Phoebe Tang <49921653+adironene@users.noreply.github.com>
2024-05-06 22:40:08 -0700

    finished speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,19 +1,42 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+var voices;
 const synth = window.speechSynthesis;
 
 function init() {
   populateVoiceList();
-  updateFace();
+
+  const pressToTalk = (document.getElementById("explore")).querySelector("button");
+  const textarea = (document.getElementById("explore")).querySelector("textarea");
+  pressToTalk.addEventListener('click', () => {
+
+    // credit: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
+    let inputText = textarea.value;
+    let utterThis = new SpeechSynthesisUtterance(inputText);
+
+    let voiceSelect = document.getElementById("voice-select")
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    // console.log("selected option is" + selectedOption);
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+
+    synth.speak(utterThis);
+    updateFace();
+  })
 
 }
+
+// credit: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
 function populateVoiceList() {
+  voices = speechSynthesis.getVoices();
   if (typeof speechSynthesis === "undefined") {
     return;
   }
-
-  const voices = speechSynthesis.getVoices();
 
   for (let i = 0; i < voices.length; i++) {
     const option = document.createElement("option");
@@ -29,10 +52,11 @@ function populateVoiceList() {
 }
 
 function updateFace() {
+  // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speaking
   const face_img = (document.getElementById("explore")).querySelector("img");
   setTimeout(() => {
     if (synth.speaking) {
-      // console.log
+  
       face_img.setAttribute('src', 'assets/images/smiling-open.png');
       updateFace();
     } else {
